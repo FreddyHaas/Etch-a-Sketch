0b62c1bbe46cf05b7ada6102892fd1d16e18215d
Freddy Haas <email@frederik-haas.de>
2022-10-18 13:54:58 +0200

    Create grid

---
+++ main.js
@@ -0,0 +1,8 @@
+const container = document.getElementById('container');
+
+for (i = 0; i < 256 ; i++) {
+        const div = document.createElement('div');
+        div.setAttribute('id','box');
+        container.appendChild(div);
+    };
+
