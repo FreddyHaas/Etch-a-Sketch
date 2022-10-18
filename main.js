8cccbcf754d7f771c0116429540fb35963cdadc1
Freddy Haas <email@frederik-haas.de>
2022-10-18 15:23:25 +0200

    Allow grit to be flexible by using Flexbox

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-
 /* Create grit */
 const container = document.getElementById('container');
-for (i = 0; i < 256 ; i++) {
+for (i = 0; i < 16; i++) {
         const div = document.createElement('div');
-        div.classList.add('box');
+        div.classList.add('column');
         container.appendChild(div);
     };
 
+const columns = document.querySelectorAll('.column');
+
+columns.forEach((column) => {
+    for (i = 0; i < 16; i++) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        column.appendChild(box);
+    }
+});
+
 /* Update color when hovering */
+
 const boxes = document.querySelectorAll('.box');
 
 boxes.forEach((box) => {
