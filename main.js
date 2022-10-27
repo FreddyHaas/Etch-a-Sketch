7f8c3acc1717fc0ea87062830be1e3d546afd632
Freddy Haas <email@frederik-haas.de>
2022-10-27 07:46:30 +0200

    Fix bug within 'removeGrit' function

--- main.js
+++ main.js
@@ -1,26 +1,66 @@
-/* Create grit */
+/* FUNCTION: Create grit */
 const container = document.getElementById('container');
-for (i = 0; i < 16; i++) {
+function createGrit (noOfRows) {
+    for (i = 0; i < noOfRows; i++) {
         const div = document.createElement('div');
         div.classList.add('column');
         container.appendChild(div);
     };
 
-const columns = document.querySelectorAll('.column');
+    const columns = document.querySelectorAll('.column');
 
-columns.forEach((column) => {
-    for (i = 0; i < 16; i++) {
-        const box = document.createElement('div');
-        box.classList.add('box');
-        column.appendChild(box);
+    columns.forEach((column) => {
+        for (i = 0; i < noOfRows; i++) {
+            const box = document.createElement('div');
+            box.classList.add('box');
+            column.appendChild(box);
     }
 });
+}
+
+/* Create initial grit */
+let gritSize = 16;
+createGrit (gritSize);
 
 /* Update color when hovering */
-
 const boxes = document.querySelectorAll('.box');
-
 boxes.forEach((box) => {
     box.addEventListener ('mouseover', () => {
         box.setAttribute('id', 'boxEdited');
     })});
+
+/* FUNCTION: Remove initial grit */
+const columns = document.querySelectorAll('.column');
+function removeGrit () {
+    columns.forEach((column) => {
+        while (column.firstChild) {
+            column.removeChild(column.lastChild);        
+        }
+    })
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+
+};
+
+/* Prompt user for input of grit size */
+let userInput;
+
+const btn = document.querySelector('button');
+btn.addEventListener ('click', () => {
+    userInput = prompt ("Please enter grit size");
+    userInput = parseInt (userInput);
+    while (!(Number.isInteger(userInput) && userInput < 101)) {
+        userInput = prompt ("Please enter number from 1 to 100");
+        userInput = parseInt (userInput);
+    }
+    gritSize = userInput;
+    removeGrit();
+    createGrit(gritSize);
+    
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
+        box.addEventListener ('mouseover', () => {
+            box.setAttribute('id', 'boxEdited');
+    })});
+});
